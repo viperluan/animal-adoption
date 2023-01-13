961947a9130aabf361a0c1fbde7a5e8191476f6d
luancs <luan.soares.ext@infracommerce.com.br>
2023-01-13 01:14:47 -0300

    feat: Create user and institutions table.

---
+++ database/migrations/2023_01_13_032341_create_users_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->uuid('id')->primary()->unique();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('cpf')->unique();
+            $table->date('birthday');
+            $table->string('image')->nullable();
+            $table->foreignUuid('address_id')->references('id')->on('adresses');
+            $table->timestampTz('email_verified_at')->nullable();
+            $table->timestampsTz();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users', function (Blueprint $table) {
+            $table->dropForeign('users_address_id_foreign');
+        });
+    }
+};
