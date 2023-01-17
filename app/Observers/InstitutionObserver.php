3e44e00e96b38d37ca46608614344405dae77363
luancs <luan.soares.ext@infracommerce.com.br>
2023-01-17 15:57:41 -0300

    feat: Create institution model and observer.

---
+++ app/Observers/InstitutionObserver.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Institution;
+use Ramsey\Uuid\Uuid;
+
+class InstitutionObserver
+{
+    /**
+     * Handle the Institution "creating" event.
+     *
+     * @param  \App\Models\Institution  $institution
+     * @return void
+     */
+    public function creating(Institution $institution)
+    {
+        $institution->id = Uuid::uuid4();
+    }
+
+    /**
+     * Handle the Institution "created" event.
+     *
+     * @param  \App\Models\Institution  $institution
+     * @return void
+     */
+    public function created(Institution $institution)
+    {
+        //
+    }
+
+    /**
+     * Handle the Institution "updated" event.
+     *
+     * @param  \App\Models\Institution  $institution
+     * @return void
+     */
+    public function updated(Institution $institution)
+    {
+        //
+    }
+
+    /**
+     * Handle the Institution "deleted" event.
+     *
+     * @param  \App\Models\Institution  $institution
+     * @return void
+     */
+    public function deleted(Institution $institution)
+    {
+        //
+    }
+
+    /**
+     * Handle the Institution "restored" event.
+     *
+     * @param  \App\Models\Institution  $institution
+     * @return void
+     */
+    public function restored(Institution $institution)
+    {
+        //
+    }
+
+    /**
+     * Handle the Institution "force deleted" event.
+     *
+     * @param  \App\Models\Institution  $institution
+     * @return void
+     */
+    public function forceDeleted(Institution $institution)
+    {
+        //
+    }
+}
