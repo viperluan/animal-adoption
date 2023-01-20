7e97ee004db42e7aed2f081dd35793a70344db19
luancs <luan.soares.ext@infracommerce.com.br>
2023-01-19 23:59:08 -0300

    feat: Created sign requests.

---
+++ app/Http/Requests/SignInRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SignInRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'email.required' => 'O email é obrigatório',
+            'password.required' => 'A senha é obrigatória',
+        ];
+    }
+}
