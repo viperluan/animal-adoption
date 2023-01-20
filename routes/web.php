b5e1d1cb05d4f3e84087957bf4b5835573ca86f5
luancs <luan.soares.ext@infracommerce.com.br>
2023-01-19 23:55:59 -0300

    refactor: Include dashboard routes and redirect the root route.

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 include "SignInRoutes.php";
 include "SignUpRoutes.php";
+include "DashboardRoutes.php";
 
 /*
 |--------------------------------------------------------------------------
@@ -15,3 +16,7 @@ include "SignUpRoutes.php";
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('/', function () {
+    return redirect('/dashboard');
+});
