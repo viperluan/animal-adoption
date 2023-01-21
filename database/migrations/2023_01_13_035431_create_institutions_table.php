0c7daad4cc6d407bd7df16c79935d3563d146224
luancs <luan.soares.ext@infracommerce.com.br>
2023-01-20 22:25:18 -0300

    refactor: Changed institution columns.

--- database/migrations/2023_01_13_035431_create_institutions_table.php
+++ database/migrations/2023_01_13_035431_create_institutions_table.php
@@ -21,8 +21,8 @@ return new class extends Migration
             $table->string('cnpj')->unique();
             $table->string('image')->nullable();
             $table->string('description')->nullable();
-            $table->foreignUuid('address_id')->references('id')->on('adresses');
-            $table->rememberToken();
+            $table->foreignUuid('address_id')->nullable()->references('id')->on('adresses');
+            $table->rememberToken()->nullable();
             $table->timestampTz('email_verified_at')->nullable();
             $table->timestampsTz();
         });
