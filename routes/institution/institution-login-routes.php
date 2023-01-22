2e718d31923bc6a950f55560924c13734fdc6aa2
luancs <luan.soares.ext@infracommerce.com.br>
2023-01-22 03:12:19 -0300

    feat: Named route of institution login.

--- routes/institution/institution-login-routes.php
+++ routes/institution/institution-login-routes.php
@@ -3,6 +3,6 @@
 use App\Http\Controllers\Institution\InstitutionLoginController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/institution/login', [InstitutionLoginController::class, 'create'])->middleware('guest:web,institution');
+Route::get('/institution/login', [InstitutionLoginController::class, 'create'])->middleware('guest:web,institution')->name('institution.login');
 Route::post('/institution/login', [InstitutionLoginController::class, 'store'])->middleware('guest:web,institution');
 Route::post('/institution/logout', [InstitutionLoginController::class, 'logout'])->middleware(['auth:institution']);
