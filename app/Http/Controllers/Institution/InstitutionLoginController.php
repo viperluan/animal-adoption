27f537b64d2d42a2ebba405c4193fb6a3f666e16
luancs <luan.soares.ext@infracommerce.com.br>
2023-01-22 01:31:30 -0300

    feat: Created institution and user controllers.

---
+++ app/Http/Controllers/Institution/InstitutionLoginController.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Http\Controllers\Institution;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Institution\InstitutionLoginRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class InstitutionLoginController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('institution.institution-login');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(InstitutionLoginRequest $request)
+    {
+        $credentials = $request->validated();
+
+        if (Auth::guard('institution')->attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return redirect()->intended('/institution/dashboard');
+        }
+
+        return back()->withErrors([
+            'credentials' => 'E-mail ou senha incorretos.'
+        ])->onlyInput('email');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(InstitutionLoginRequest $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function logout(Request $request)
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/');
+    }
+}
