ef742010f8fb3dd7998a3b5879d117f5cdc276ae
luancs <luan.soares.ext@infracommerce.com.br>
2023-01-26 00:54:54 -0300

    refactor: Changed files names and header institution dashboard.

--- resources/views/institution/institution-dashboard.blade.php
+++ resources/views/institution/institution-dashboard.blade.php
@@ -4,19 +4,23 @@
 
 @section('content')
     <div class="institution-dashboard-page">
-        <a href="/">
-            <button>Voltar</button>
-        </a>
+        <header class="institution-dashboard-header">
+            <a href="/">
+                <button class="institution-dashboard-button">Voltar</button>
+            </a>
+
+            <form action="/institution/logout" method="POST">
+                @csrf
+
+                <button class="institution-dashboard-button" type="submit">Logout</button>
+            </form>
+        </header>
 
         @auth
             <p>Seja bem vindo, {{ auth()->user()->name }}!</p>
         @endauth
 
-        <form action="/institution/logout" method="POST">
-            @csrf
 
-            <button type="submit">Logout</button>
-        </form>
     </div>
 
 @endsection
