09e22f7c965b52e1f8f26d0bad3baee58c551f56
luancs <luan.soares.ext@infracommerce.com.br>
2023-01-26 17:26:32 -0300

    feat: Create controllers and import to views.

--- routes/institution/dashboard-routes.php
+++ routes/institution/dashboard-routes.php
@@ -1,6 +1,16 @@
 <?php
 
+use App\Http\Controllers\Institution\InstitutionDashboardAddressController;
+use App\Http\Controllers\Institution\InstitutionDashboardAdoptionsController;
+use App\Http\Controllers\Institution\InstitutionDashboardAnimalsController;
 use App\Http\Controllers\Institution\InstitutionDashboardController;
+use App\Http\Controllers\Institution\InstitutionDashboardInfoController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/institution/dashboard', [InstitutionDashboardController::class, 'create'])->middleware(['auth:institution', 'verified']);
+Route::middleware(['auth:institution', 'verified'])->group(function () {
+    Route::get('/institution/dashboard', [InstitutionDashboardController::class, 'create']);
+    Route::get('/institution/dashboard/info', [InstitutionDashboardInfoController::class, 'create']);
+    Route::get('/institution/dashboard/address', [InstitutionDashboardAddressController::class, 'create']);
+    Route::get('/institution/dashboard/adoptions', [InstitutionDashboardAdoptionsController::class, 'create']);
+    Route::get('/institution/dashboard/animals', [InstitutionDashboardAnimalsController::class, 'create']);
+});
