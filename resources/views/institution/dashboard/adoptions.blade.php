8c603a5bdcdab918d00a4c268aba0714077ae87e
luancs <luan.soares.ext@infracommerce.com.br>
2023-01-26 17:25:54 -0300

    feat: Create section views.

---
+++ resources/views/institution/dashboard/adoptions.blade.php
@@ -0,0 +1,37 @@
+@extends('index')
+
+@section('title', 'Instituição - Adoções')
+
+@section('content')
+    <div class="institution-dashboard-page">
+        <header class="institution-dashboard-header">
+            <a href="/institution/dashboard">
+                <button class="institution-dashboard-button">Voltar</button>
+            </a>
+        </header>
+
+        <main class="institution-dashboard-main">
+            <aside class="institution-dashboard-aside">
+                <ul>
+                    <li>
+                        <a href="/institution/dashboard/info">Dados da instituição</a>
+                    </li>
+                    <li>
+                        <a href="/institution/dashboard/address">Endereço</a>
+                    </li>
+                    <li>
+                        <a href="/institution/dashboard/adoptions">Adoções</a>
+                    </li>
+                    <li>
+                        <a href="/institution/dashboard/animals">Animais cadastrados</a>
+                    </li>
+                </ul>
+            </aside>
+
+            @auth
+                <p>Adoções da institutição</p>
+            @endauth
+        </main>
+    </div>
+
+@endsection
